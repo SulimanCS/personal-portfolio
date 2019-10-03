35b1a3d1a57e5111f03e3421b47424a543c2f680
Suliman Alsarraf <suliman2@pdx.edu>
2019-10-02 18:20:50 -0700

    Applied the Typed.js effect on the sub title

--- js/script.js
+++ js/script.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
 		pagination: false
 	});
 
+	var typed = new Typed(".typed", {
+		strings: ["Computer Scientist.", 
+		"Developer.", "2020 Graduate."],
+		typeSpeed: 40,
+		loop: true,
+		startDelay: 750,
+		showCursor: false
+	});
+	
 });
